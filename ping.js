806d688107f4d17c4da428f6427882a110c54eb2
Serena_Serena <serena.kifoula@epitech.eu>
2024-05-30 15:41:09 +0200

    Add: ping files

--- ping.js
+++ ping.js
@@ -1,24 +1,10 @@
-const fs = require("node:fs");
-const path = require("node:path");
-const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
-const { token } = require("./config.json");
+const { SlashCommandBuilder } = require("discord.js");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.commands = new Collection();
-
-const foldersPath = path.join(__dirname, "commands");
-const commandFiles = fs
-    .readdirSync(foldersPath)
-    .filter((file) => file.endsWith(".js"));
-for (const file of commandFiles) {
-    const filePath = path.join(foldersPath, file);
-    const command = require(filePath);
-    if ("data" in command && "execute" in command) {
-        client.commands.set(command.data.name, command);
-    } else {
-        console.log(
-            `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
-        );
-    }
-}
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("ping")
+        .setDescription("reply with pong"),
+    async execute(interaction) {
+        await interaction.reply(`pong`);
+    },
+  };
